a43e420200ea982f057628cb13372571a3e31f0f
chachcha <khushi.chachcha@rogers.com>
2012-12-02 16:13:52 -0800

    details of response added, cookies and date still pending

--- service.c
+++ service.c
@@ -34,7 +34,10 @@ void handle_client(int socket) {
 	//char* cookie_value = http_parse_header_field(buffer, strlen("Connection"), "Connection");
 	parse_request(buffer, &request, len);
 	build_response(&request, &response);
-	print_response(&response);
+	if (!all_params_present(&request))
+		forbidden_command();
+	else if (request.command != NOTA)
+		print_response(&response);
 	send(socket,buffer, strlen(buffer), 0);
 } 
 
@@ -46,6 +49,7 @@ void parse_request(char* buffer, request_info* request, int len){
 	request->host = http_parse_header_field(buffer, len, "Host");
 	request->user_agent = http_parse_header_field(buffer, len, "User-Agent");
 	request->content_length = http_parse_header_field(buffer, len, "Content-Length");
+	request->transfer_encoding = http_parse_header_field(buffer, len, "Transfer-Encoding");
 	request->parameters = http_parse_path(http_parse_uri(buffer));
 
 }
@@ -60,40 +64,87 @@ command_type parse_command(char* uri){
 }
 
 void build_response(request_info* request, response_info* response){
+	char* location= "";
+	response->info = request;
+	response->userID = NULL;
+	response->last_modified = NULL;
+	if (response->info->cacheControl == NULL)
+		response->info->cacheControl = "public"; //TODO: check this is true
 	switch(request->command){
 		case LOGIN:
 			break;
 		case LOGOUT:
 			break;
 		case SERVERTIME:
+			response->info->cacheControl = "no-cache";
 			break;
+		//browser seems to be all done
 		case BROWSER:
+			response->info->cacheControl = "private";
+			printf("User-Agent: %s\n", response->info->user_agent);
 			break;
 		case REDIRECT:
+			//TODO:return HTTP with the code for "See Other" (I don't know what that means yet)
+			printf("Location: %s\n", extract_parameter("url=", decode(response->info->parameters, location)));
 			break;
 		case GET_FILE:
+			//if success
+			response->info->content_type = "application/octet-stream";
+			//set response->last_modified;
 			break;
 		case PUT_FILE:
+			response->info->cacheControl = "no-cache";
 			break;
 		case ADD_CART:
 			break;
 		case DEL_CART:
 			break;
 		case CHECKOUT:
+			if (response->userID == NULL)
+				printf("User must be logged in to checkout."); 
 			break;
 		case CLOSE:
+			response->info->connection = "close";
+			printf("The connection will now be closed.");
 			break;
 		case NOTA:
+			//TODO: return HTTP response code for Not Found here
+			printf("Command not found");
 			break;
 	}
-	response->info = request;
+	if (response->info->content_type == NULL)
+		response->info->content_type = "text/plain";
 	response->date = "";
+}
+
+//TODO: implement this
+char* extract_parameter(char* param, char* string){
+	//find param in string	
+	//return the string that follows until you find '&' or ' '
+	return "";
+}
+
+bool all_params_present(request_info* request){
+	char* location = "";	
+	if ((request->command == BROWSER && request->user_agent == NULL) ||
+	    (request->command == REDIRECT && extract_parameter("url=", decode(request->parameters,location))))
+		return false;
+	return true;
+}
+
+void forbidden_command(){
+	//TODO: return HTTP response code for Forbidden here
+	printf("Command not found");
 }
 
 void print_response(response_info* response){
 	printf("Connection: %s\n", response->info->connection);
     	printf("Cache-Control: '%s'\n", response->info->cacheControl);
+	//TODO: find out which one of the following two to display when	
 	printf("Content-length: '%s'\n", response->info->content_length);
+	printf("Transfer-Encoding: '%s'\n", response->info->transfer_encoding);
     	printf("Content-Type: '%s'\n", response->info->content_type);
     	printf("Date: '%s'\n", response->date);
+	if (response->info->command == GET_FILE)
+		printf("Last-Modified: '%s'\n", response->last_modified);
 }
--- service.h
+++ service.h
@@ -6,6 +6,7 @@
 #define _SERVICE_H_
 
 #include "util.h"
+#include <stdbool.h>
 
 typedef enum {
     LOGIN, LOGOUT, SERVERTIME, BROWSER,
@@ -13,25 +14,16 @@ typedef enum {
     DEL_CART, CHECKOUT, CLOSE, NOTA
 } command_type;
 
-//don't think we need this anymore
-typedef enum {
-    NO_CACHE, PRIVATE, PUBLIC
-} cache_control;
-
-typedef enum {
-    KEEP_ALIVE
-} connection_type;
-
 typedef struct request_info{
 	http_method req_type; //do we really need this?
 	command_type command;
-	double http_version; //the util class doesn't have any funciton to extract this so that makes me think maybe we don't need it?
 	char* cacheControl;
 	char* connection;
 	char* host;
 	char* user_agent;
 	char* content_length;
 	char* content_type;
+	char* transfer_encoding;
 	const char* parameters;
 } request_info;
 
@@ -39,12 +31,16 @@ typedef struct response_info{
 	struct request_info* info;
 	char* date;
 	char* userID;
+	char* last_modified;
 } response_info;
 
 void handle_client(int socket);
 void parse_request(char* buffer, request_info* request, int len);
 command_type parse_command(char* uri);
 void build_response(request_info* request, response_info* response);
+char* extract_parameter(char* param, char* string);
 void print_response(response_info* response);
+void forbidden_command();
+bool all_params_present(request_info* request);
 
 #endif
